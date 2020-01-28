1656744e7c0966d5e75d1f96eafa28f376dfc282
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2020-01-28 10:16:02 -0800

    created controller with routes.  Started adding config files

---
+++ src/main/java/com/micahthor/codefellowship/models/ApplicationUser.java
@@ -0,0 +1,94 @@
+package com.micahthor.codefellowship.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+public class ApplicationUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    private String userName;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String bio;
+    private URL profilePicture;
+
+    public ApplicationUser(String userName, String password, String firstName, String lastName, Date dateOfBirth, String bio, URL profilePicture) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
+        this.profilePicture = profilePicture;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public URL getProfilePicture() {
+        return profilePicture;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
